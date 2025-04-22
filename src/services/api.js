6e669777d7273b9a4184ef2b67d17a04c9834d7f
Aayush Thakur <athakur.1705096@gmail.com>
2025-04-21 22:59:44 -0400

    update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.REACT_APP_BACKEND_URL || 'http://localhost:5000', // Fallback for local
+  baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000', // Fallback for local
   withCredentials: false, // set to true only if you use cookies/sessions
 });
 
